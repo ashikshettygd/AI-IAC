e3daa0c6f32d6e4e6c43c42ea44235d2924958c7
ashikshettygd <ashetty@griddynamics.com>
2025-10-17 02:07:00 +0530

    IaC update: storage at dev-1760647012

---
+++ storage/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "free_tier_bucket" {
+  bucket = "free-tier-bucket-${random_string.suffix.id}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Free Tier S3 Bucket"
+    Environment = "Dev"
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.free_tier_bucket.id
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.free_tier_bucket.arn
+}
